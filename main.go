0a58bdc51e797ccff255061ac5470e81c38f2118
Hamid Azimi <activelord@gmail.com>
2019-09-30 23:14:58 +0330

    install echo

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main()  {
+	e := echo.New()
+
+	e.GET("/hello", func(context echo.Context) error {
+		return context.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
