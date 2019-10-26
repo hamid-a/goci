ca7b4ddcafb8524609ac77e7e76ca033da642a1b
Hamid Azimi <activelord@gmail.com>
2019-10-26 13:51:19 +0330

    deployment pipelines added

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/spf13/viper v1.4.0 // indirect
 	golang.org/x/crypto v0.0.0-20190927123631-a832865fa7ad // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,74 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
+	"github.com/spf13/viper"
 	"net/http"
+	"os/exec"
 )
 
-func main()  {
-	e := echo.New()
+var e = echo.New()
 
-	e.GET("/hello", func(context echo.Context) error {
-		return context.String(http.StatusOK, "Hello, World!")
+func main() {
+	e.HideBanner = true
+
+	viper.SetConfigName("pipeline")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		e.Logger.Fatalf("Fatal error config file: %s", err.Error())
+	}
+
+	e.GET("/health", func(context echo.Context) error {
+		return context.String(http.StatusOK, "It works!")
 	})
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.POST("/events", HandleEvents)
+
+	e.Logger.SetLevel(log.DEBUG)
+	e.Logger.Fatal(e.Start(":" + viper.GetString("port")))
+}
+
+func HandleEvents(c echo.Context) error {
+
+	token := c.Request().Header.Get("X-Gitlab-Token")
+	service := c.QueryParam("service")
+	accessToken := viper.GetString("token." + service)
+
+	if accessToken != token {
+		return c.String(http.StatusUnauthorized, "Unauthorized.")
+	}
+
+	go runPipeline(service)
+
+	return c.String(http.StatusOK, "ok.")
+}
+
+func runPipeline(service string) {
+
+	for _, pipeline := range viper.GetStringSlice(service + ".pipelines") {
+		key := fmt.Sprintf("%s.%s.", service, pipeline)
+		dir := viper.GetString(key + "directory")
+		for _, command := range viper.GetStringSlice(key + "commands") {
+
+			cmd := exec.Command("bash", "-c", command)
+			cmd.Dir = dir
+			out, err := cmd.Output()
+
+			e.Logger.Debug(fmt.Printf(
+				"Service: %s, Command: %s, Path: %s, Message: %s",
+				service,
+				command,
+				dir,
+				string(out),
+			))
+
+			if err != nil {
+				e.Logger.Error(err)
+			}
+		}
+	}
 }
